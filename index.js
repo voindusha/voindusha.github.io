98b58164765d35579da3089e4ac37b63729cd336
Ahmad Kenzie Aryasatya <132659264+voindusha@users.noreply.github.com>
2024-06-04 13:57:22 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const abc1 = "thequickbrownfoxjumpsoverthelazydog"
+const abc = "abcdefghijklmnopqrstuvwxyz"
+const key = document.getElementById("key")
+const input = document.getElementById("input")
+const encrypt = document.querySelector("button:nth-of-type(1)")
+const decrypt = document.querySelector("button:nth-of-type(2)")
+const output = document.querySelector("section")
+
